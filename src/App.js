b4d32d83716a8c653efa69cffc1e81463436ccfe
Anastasia Karpenko <anastasia.karpenko@relexsolutions.com>
2020-05-31 18:33:21 +0300

    Adding List component

--- src/App.js
+++ src/App.js
@@ -39,18 +39,28 @@ function App() {
 
       {list.map(function (item) {
         return (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
+          <div>
+            <List />
           </div>
         );
       })}
     </div>
   );
+}
+
+function List() {
+  return list.map(function (item) {
+    return (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+    );
+  });
 }
 
 export default App;
