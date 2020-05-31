2bdd99c70b881c6aef553c7aaff5f3c621940614
Anastasia Karpenko <anastasia.karpenko@relexsolutions.com>
2020-05-31 17:07:46 +0300

    Using lists in JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import React from 'react';
 
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
-};
-
-function getTitle(title) {
-  return title;
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+  {
+    title: 'Kotlin',
+    url: 'https://kotlinlang.org/',
+    author: 'JetBrains',
+    num_comments: 4,
+    points: 10,
+    objectID: 2,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>
-        {welcome.greeting} {getTitle(welcome.title)}
-      </h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+
+      {list.map(function (item) {
+        return (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        );
+      })}
     </div>
   );
 }
